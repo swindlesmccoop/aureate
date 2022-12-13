d9005e1a798270afccdbe3808fe96f01a31113a4
swindlesmccoop <swindlesmccoop@waifu.club>
2022-12-13 19:07:19 +0000

    Need to set proper output dir still

--- aureate.c
+++ aureate.c
@@ -1,22 +1,28 @@
+#include <archive.h>
+#include <archive_entry.h>
+#include <curl/curl.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl/curl.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <tar.h>
+#include <unistd.h>
 
 int download(char *argv[]) {
 	//get cache dir
-	char cache_dir[100];
-    strcpy(cache_dir, getenv("XDG_CACHE_HOME"));
+	char syscache[100];
+    strcpy(syscache, getenv("XDG_CACHE_HOME"));
 	struct stat st;
-	if (stat(cache_dir, &st) == -1) {mkdir(cache_dir, 0700);}
-    strcat(cache_dir, "/aureate/");
+	if (stat(syscache, &st) == -1) {
+		mkdir(syscache, 0700);
+	}
+    strcat(syscache, "/aureate/");
     char cache[100];
-    strcpy(cache, cache_dir);
-	if (stat(cache, &st) == -1) {mkdir(cache, 0700);}
+    strcpy(cache, syscache);
+	if (stat(cache, &st) == -1) {
+		mkdir(cache, 0700);
+	}
 
 	//get URL
 	const char* pkg = argv[2];
@@ -27,7 +33,6 @@ int download(char *argv[]) {
 	char final_url[100] = "";
 	strcat(final_url, url);
 	strcat(final_url, ".tar.gz");
-	//printf("Final URL: %s\n", final_url);
 	
 	//curl file	
 	CURL *curl;
@@ -46,11 +51,39 @@ int download(char *argv[]) {
 		fclose(fp);
 	}
 
-	const char* tarfile = strcat(cache, pkg);
-	tar_open(&tarfile, tarfile, NULL, O_RDONLY, 0, 0);
-	tar_extract_all(tarfile, "./");
-	tar_close(tarfile);
-	
+	char* tarfile = strcat(cache, pkg);
+	strcat(tarfile, ".tar.gz");
+	struct archive *a = archive_read_new();
+	archive_read_support_filter_gzip(a);
+	archive_read_support_format_tar(a);
+	int r = archive_read_open_filename(a, tarfile, 10240);
+	if (r != ARCHIVE_OK) {
+		fprintf(stderr, "Not a tar archive.\n");
+		return 1;
+	}
+
+	struct archive *ext = archive_write_disk_new();
+	archive_write_disk_set_options(ext, ARCHIVE_EXTRACT_SECURE_NODOTDOT | ARCHIVE_EXTRACT_SECURE_SYMLINKS | ARCHIVE_EXTRACT_PERM);
+	archive_write_disk_set_standard_lookup(ext);
+	archive_write_disk_set_options(ext, cache);
+
+	struct archive_entry *entry;
+	while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
+		const char *path = archive_entry_pathname(entry);
+		int size = archive_entry_size(entry);
+		r = archive_read_extract(a, entry, 0);
+		if (r != ARCHIVE_OK) {
+			fprintf(stderr, "Placeholder tar extract error.\n");
+			return 1;
+		}
+	}
+
+	archive_read_close(a);
+	archive_read_free(a);
+	archive_write_close(ext);
+	archive_write_free(ext);
+	printf("Done\n");
+
 	return 0;
 }
 
