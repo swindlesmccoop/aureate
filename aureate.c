237dfbd34376c4f0ce89d71eaeebb0e1f2aaa781
swindlesmccoop <swindlesmccoop@waifu.club>
2022-12-18 06:31:40 +0000

    Use sprintf instead of strncat

--- aureate.c
+++ aureate.c
@@ -13,53 +13,66 @@
 #define SUFFIX ".git"
 
 int download(char *argv[]) {
+	//define vars
 	const char* syscache = getenv("XDG_CACHE_HOME");
 	const char* pkg = argv[2];
 
+	//check if XDG_CACHE_HOME exists - if not, create it
 	struct stat st;
-	
 	if (stat(syscache, &st) == -1) {
 		mkdir(syscache, 0700);
 	}
 
-	char cache[strlen(syscache)+strlen("/aureate")+1];
-	memset(cache, 0, strlen(syscache)+strlen("/aureate")+1);
-	strncat(cache, syscache, strlen(syscache));
-	strncat(cache, "/aureate", strlen("/aureate"));
+	//construct aureate cache var
+	char cache[strlen(syscache)+strlen("aureate")+1];
+	memset(cache, 0, strlen(syscache)+strlen("aureate")+1);
+	sprintf(cache, "%s/%s", syscache, "aureate");
+	
+	//do the same thing as syscache with aureate cache
 	if (stat(cache, &st) == -1) {
 		mkdir(cache, 0700);
 	}
 
-	//get URL
+	//construct clone URL
 	char clone_url[strlen(BASE_URL)+strlen(pkg)+strlen(SUFFIX)+1];
 	memset(clone_url, 0, strlen(BASE_URL)+strlen(pkg)+strlen(SUFFIX)+1);
-	strncat(clone_url, BASE_URL, strlen(BASE_URL));
-	strncat(clone_url, pkg, strlen(pkg));
-	strncat(clone_url, SUFFIX, strlen(SUFFIX));
+	sprintf(clone_url, "%s%s%s", BASE_URL, pkg, SUFFIX);
 
+	//clone or pull the repo
 	chdir(cache);
 	git_libgit2_init();
-	printf(BLUE ":: " RESET "Fetching repo...");
-	fflush(stdout);
+
+	//if directory for package already exists, clone
 	if (stat(pkg, &st) == -1) {
+		printf(BLUE ":: " RESET "Fetching repo..."); fflush(stdout);
 		git_repository *repo = NULL;
 		git_clone(&repo, clone_url, pkg, NULL);
 		chdir(pkg);
 		if (access("PKGBUILD", F_OK) != -1) {
-			printf("done.\n");
-			fflush(stdout);
+			printf("done.\n"); fflush(stdout);
 		} else {
-			printf("error. Package name invalid or you are not connected to the internet.\n");
-			fflush(stdout);
+			printf("error. Package name invalid or you are not connected to the internet.\n"); fflush(stdout);
+			//remove failed clone
 			return 1;
 		}
+	//else, pull latest changes
 	} else {
+		printf(BLUE ":: " RESET "Fetching latest changes..."); fflush(stdout);
+		git_repository *repo;
+		git_remote *remote;
+
+		git_repository_open(&repo, pkg);
+		git_remote_lookup(&remote, repo, "origin");
+		git_remote_fetch(remote, NULL, NULL, "pull");
+		git_remote_free(remote);
+		git_repository_free(repo);
+		
+		printf("done.\n"); fflush(stdout);
 		chdir(pkg);
-		//git fetch code
 	}
 
+	//hand off the rest to pacman
 	system("makepkg -si");
-
 	return 0;
 }
 
